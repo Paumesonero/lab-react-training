c7c0ee4ca8bdd73ba050f525b1b304edbc639dbb
Pau Mesonero <paumesonero@Caitlins-MacBook-Air.local>
2022-07-23 22:40:16 +0200

    iterations 0-1 done

---
+++ src/components/IdCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function IdCard(props) {
+    console.log(props)
+    return (
+        <div className='user-card-container'>
+            <img src={props.picture} alt="user" />
+            <div>
+                <p><strong>First name:</strong>{props.firstName}</p>
+                <p><strong>Last name:</strong>{props.lastName}</p>
+                <p><strong>Gender:</strong>{props.gender}</p>
+                <p><strong>Height:</strong>{props.height}</p>
+                <p><strong>Birth:</strong>{props.birth}</p>
+
+            </div>
+        </div>
+    )
+}
